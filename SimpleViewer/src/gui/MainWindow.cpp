1e9906e31cee0fa709e413213ac9dec9fe172625
Bernd Hahnebach <bernd@bimstatik.org>
2015-01-19 07:53:06 +0100

    [Doc] set Windowtitle

--- SimpleViewer/src/gui/MainWindow.cpp
+++ SimpleViewer/src/gui/MainWindow.cpp
@@ -32,7 +32,7 @@
 MainWindow::MainWindow( IfcPlusPlusSystem* sys, ViewerWidget* vw, QWidget *parent) : m_system(sys), m_viewer_widget(vw), QMainWindow(parent)
 {
 	m_system = sys;
-	setWindowTitle("IFC++ Viewer");
+	setWindowTitle("IFC++ Viewer R30 bhb outputs");
 	setWindowIcon( QIcon( ":img/IfcPlusPlusViewerWindowIcon.png" ) );
 	
 	// global style sheet definitions
